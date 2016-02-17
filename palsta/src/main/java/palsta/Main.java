b718adc0cc19205ba2eb445da507047b01211b68
Vesa Riekkinen <vesa.riekkinen@gmail.com>
2016-02-17 15:35:17 +0200

    Tietokannan lataus (kanta vielä tyhjä)

--- palsta/src/main/java/palsta/Main.java
+++ palsta/src/main/java/palsta/Main.java
@@ -5,13 +5,15 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:???.db");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:../tietokannat/keskustelut.db");
         Statement stmt = connection.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM Alue");
 
-        while (rs.next()) {
-            System.out.println(rs.getString("nimi"));
-        }
+        System.out.println("rs.next() palauttaa " + rs.next());
+        
+//        while (rs.next()) {
+//            System.out.println(rs.getString("nimi"));
+//        }
 
         rs.close();
         stmt.close();
