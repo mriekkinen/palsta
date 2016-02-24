10f8022ce8349baac74efab657649875d0ef9152
Kiirikki Anne M <akiirikk@da5-cs-b221-27.helsinki.fi>
2016-02-24 15:10:15 +0200

    testi

--- palsta/src/main/java/palsta/pojo/Keskustelu.java
+++ palsta/src/main/java/palsta/pojo/Keskustelu.java
@@ -1,7 +1,38 @@
 package palsta.pojo;
 
+import java.util.*;
+import java.sql.*;
+
 public class Keskustelu {
+
+    private Integer tunnus;
+    private Integer alue; // Alueen PK
+    private String webTunnus;
+    private String otsikko;
+
+    public Keskustelu(Integer tunnus, Integer alue, String webTunnus, String otsikko) {
+        this.tunnus = tunnus;
+        this.alue = alue;
+        this.webTunnus = webTunnus;
+        this.otsikko = otsikko;
+
+    }
+
+    public Integer getTunnus() {
+        return this.tunnus;
+    }
     
+    public Integer getAlue() {
+        return this.alue;
+       
+    }
+
+    public String getWebTunnus() {
+        return this.webTunnus;
+    }
     
-    
+    public String getOtsikko() {
+        return this.otsikko;
+    } 
+
 }
