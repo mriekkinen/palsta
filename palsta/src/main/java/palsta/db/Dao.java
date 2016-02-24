1c0700a2e5c766698dd36607d95184ace6357120
Kiirikki Anne M <akiirikk@da5-cs-b221-27.helsinki.fi>
2016-02-24 16:34:52 +0200

    dao ja database katos vahingossa

---
+++ palsta/src/main/java/palsta/db/Dao.java
@@ -0,0 +1,17 @@
+
+package palsta.db;
+
+import java.sql.*;
+import java.util.*;
+
+public interface Dao<T, K> {
+
+    T findOne(K key) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    void delete(K key) throws SQLException;
+
+    void insert() throws SQLException;
+
+}
---
+++ palsta/src/main/java/palsta/db/Database.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palsta.db;
+
+/**
+ *
+ * @author akiirikk
+ */
+import java.sql.*;
+
+public class Database {
+
+    private String databaseAddress;
+
+    public Database(String databaseAddress) throws ClassNotFoundException {
+        this.databaseAddress = databaseAddress;
+   }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseAddress);
+    }
+}
