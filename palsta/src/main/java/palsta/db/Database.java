d4eded405db43c2fb54d5fe3af4b0309bcaad38e
eksola <valtteri.eskola@gmail.com>
2016-02-24 15:38:50 +0200

    AlueDao done

--- palsta/src/main/java/palsta/db/Database.java
+++ palsta/src/main/java/palsta/db/Database.java
@@ -1,7 +1,17 @@
 package palsta.db;
 
+import java.sql.*;
+
 public class Database {
     
-    
-    
+    private String databaseAddress;
+
+    public Database(String databaseAddress) throws ClassNotFoundException {
+        this.databaseAddress = databaseAddress;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseAddress);
+    }
 }
+        
