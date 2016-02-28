289337b08890450ce7b8730d44627d732402a3d4
Vesa Riekkinen <vesa.riekkinen@gmail.com>
2016-02-28 20:56:11 +0200

    Sivut alue ja keskustelu

--- palsta/src/main/java/palsta/Main2.java
+++ palsta/src/main/java/palsta/Main2.java
@@ -15,15 +15,41 @@ public class Main2 {
         // 2. Mene selaimella osoitteeseen http://localhost:4567/
         Database db = new Database("jdbc:sqlite:../tietokannat/keskustelut.db");
         AlueDao alueDao = new AlueDao(db);
+        KeskusteluDao keskusteluDao = new KeskusteluDao(db);
+
         List<Alue> alueet = alueDao.findAll();
+        List<Keskustelu> keskustelut = keskusteluDao.findAll();
+        List<Viesti> viestit = new ArrayList<>();
+
+        viestit.add(new Viesti(1, 1, "Matti", "", new Timestamp(1456740000), "Hyvin toimii"));
+        viestit.add(new Viesti(1, 1, "Mikko", "", new Timestamp(1456747200), "Jep"));
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("title", "Keskustelupalsta");
-
             map.put("alueet", alueet);
 
             return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+
+        get("/alue/:tunnus", (req, res) -> {
+            String webTunnus = req.params(":tunnus");
+
+            HashMap map = new HashMap<>();
+            map.put("title", webTunnus);
+            map.put("keskustelut", keskustelut);
+
+            return new ModelAndView(map, "alue");
+        }, new ThymeleafTemplateEngine());
+
+        get("/keskustelu/:tunnus", (req, res) -> {
+            String tunnus = req.params(":tunnus");
+
+            HashMap map = new HashMap<>();
+            map.put("title", tunnus);
+            map.put("viestit", viestit);
+
+            return new ModelAndView(map, "keskustelu");
         }, new ThymeleafTemplateEngine());
     }
 }
