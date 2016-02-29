072ffded000f80c0c022dca2a6e8d905d76d9baa
Vesa Riekkinen <vesa.riekkinen@gmail.com>
2016-02-29 15:55:04 +0200

    Linkit avaa keskustelu ja vastaa keskusteluun

--- palsta/src/main/java/palsta/Main2.java
+++ palsta/src/main/java/palsta/Main2.java
@@ -39,6 +39,7 @@ public class Main2 {
             HashMap map = new HashMap<>();
             map.put("title", webTunnus);
             map.put("keskustelut", keskustelut);
+            map.put("alueenWebTunnus", webTunnus);
 
             return new ModelAndView(map, "alue");
         }, new ThymeleafTemplateEngine());
@@ -49,6 +50,7 @@ public class Main2 {
             HashMap map = new HashMap<>();
             map.put("title", tunnus);
             map.put("viestit", viestit);
+            map.put("keskustelunTunnus", tunnus);
 
             return new ModelAndView(map, "keskustelu");
         }, new ThymeleafTemplateEngine());
@@ -63,22 +65,32 @@ public class Main2 {
 
             return new ModelAndView(map, "vastaa");
         }, new ThymeleafTemplateEngine());
-        
-        post("/vastaa/:keskustelunTunnus", (req, res) -> {
-            int tunnus = muunna(req.params(":keskustelunTunnus"));
-            Keskustelu keskustelu = keskusteluDao.findOne(tunnus);
-            
-            // keskustelu.insert(...)
 
-            return "Tämä tulee kutsumaan metodia keskustelu.insert...";
+        post("/vastaa", (req, res) -> {
+            int tunnus = muunna(req.params("keskustelunTunnus"));
+            Keskustelu keskustelu = keskusteluDao.findOne(tunnus);
+
+            // viestiDao.insert(keskustelunTunnus, lahettaja, sisalto)
+            return "Tämä tulee kutsumaan metodia viestiDao.insert...";
         });
-        
-        get("avaa", (req, res) -> {
+
+        get("avaa/:alueenWebTunnus", (req, res) -> {
+            String webTunnus = req.params(":alueenWebTunnus");
+
+            // Alue alue = alueDao.findOne( webTunnus )
             HashMap map = new HashMap<>();
             map.put("title", "Avaa keskustelu");
-            
+            //map.put("alue", alue);
+
             return new ModelAndView(map, "avaa");
         }, new ThymeleafTemplateEngine());
+
+        post("avaa", (req, res) -> {
+            String webTunnus = req.params("alueenWebTunnus");
+
+            // keskusteluDao.insert(alueenWebTunnus, otsikko, lahettaja, sisalto)
+            return "Tämä tulee kutsumaan metodia keskusteluDao.insert...";
+        });
     }
 
     private static int muunna(String merkkijono) {
